37804ba20126acc14058dca8f95525bfb46633f6
nnoldi-hub <158357372+nnoldi-hub@users.noreply.github.com>
2025-03-31 12:14:35 +0300

    Add files via upload

--- train.py
+++ train.py
@@ -1,33 +1,27 @@
-import json
-import pickle
+import json, pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-with open("intents.json", "r", encoding="utf-8") as file:
+with open("intents.json", encoding="utf-8") as file:
     data = json.load(file)
 
-training_sentences = []
-training_labels = []
+patterns = []
+tags = []
 responses = {}
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
-        training_sentences.append(pattern)
-        training_labels.append(intent["tag"])
+        patterns.append(pattern)
+        tags.append(intent["tag"])
     responses[intent["tag"]] = intent["responses"]
 
 vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(training_sentences)
-y = training_labels
+X = vectorizer.fit_transform(patterns)
+y = tags
 
 model = MultinomialNB()
 model.fit(X, y)
 
-with open("model.pkl", "wb") as file:
-    pickle.dump(model, file)
-
-with open("vectorizer.pkl", "wb") as file:
-    pickle.dump(vectorizer, file)
-
-with open("responses.pkl", "wb") as file:
-    pickle.dump(responses, file)
+pickle.dump(model, open("model.pkl", "wb"))
+pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
+pickle.dump(responses, open("responses.pkl", "wb"))
